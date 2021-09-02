ca7a751111326fc1430ac1eb547aa23945d6544b
totopc <raymarlobaton@gmail.com>
2021-09-03 00:52:58 +0800

    nav final fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,15 +18,6 @@ const Home = () =>{
     
 
     
-    function makeFocal(e){
-        e.target.className="underline  underline-green2-DEFAULT underline-style-wavy"
-
-    
-    }
-    function makeBlur(e){
-        e.target.className=""
-    }
-
 
      
 return (
@@ -39,19 +30,34 @@ return (
          <p className=" border-t-2 border-gray-800 w-40 ml-5 mt-1 bg">
         </p>
          </h3> 
-         <h1 className = "text-7xl font-russo">REDACT.DEV</h1>
+         <h1 className = "text-7xl font-russo ">REDACT.DEV</h1>
         </div>
         <ul className=" col-start-7 col-span-2 row-span-5 font-orbitron">
-  {projects.map((projects)=>(
-      <li key={projects.value} onMouseOver={()=>  newProject(projects.value)} 
-      className="flex my-5" class="nav">
-          <p className="w-5 text-xs font-black text-green2  transform  -rotate-90 mr-2 " >{projects.value}</p>
-          <p 
-          className=""><a href="#" class="wavey">{projects.name}</a></p>
+
+            
+ 
+  {projects.map((projects) => (projects.value!= defaultProject)?
+  (  
+    
+      <li key={projects.value} onMouseOver={()=> newProject(projects.value)} 
+      className="filter blur-sm flex my-5 nav"  >
+          <p className="w-5 text-xs font-black text-green2  transform  -rotate-90 mr-2" >{projects.value}</p>
+          <p><a href="#" >{projects.name}</a></p>
       </li>
-      
-  ))}
+  ):
+  (  
+    <li key={projects.value} onMouseOver={()=> newProject(projects.value)} 
+    className="flex my-5 navActive" >
+        <p className="w-5 text-xs font-black text-green2  transform  -rotate-90 mr-2 " >{projects.value}</p>
+        <p><a href="#" className="wavey">{projects.name}</a></p>
+    </li>
+    
+)
+  
+  )}
+
   {/* <h1>{defaultProject}</h1> */}
+
         </ul>
         <div className=" w-1/4 z-0 -ml-80 absolute col-start-2 col-span-4 border-2 border-gray-800 mt-5 p-5">
             <img  className="filter blur-sm" src={imgTTM} />
@@ -96,12 +102,12 @@ return (
             
            
 
-            <a className="mr-5" href="mailto:raymarlobaton@gmail.com"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <a className="mr-5" href="mailto:raymarlobaton@gmail.com"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
             </svg> </a>
 
-            <a href="https://www.youtube.com/channel/UClXBtnbCQoeAyR5AQLMLCBg"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <a href="https://www.youtube.com/channel/UClXBtnbCQoeAyR5AQLMLCBg"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
             </svg></a>
                         
